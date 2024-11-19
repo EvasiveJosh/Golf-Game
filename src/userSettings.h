786fd507c39f92f97543fb275a4bdae3f5f1f765
Thomas <21tap8@queensu.ca>
2024-11-19 04:01:41 -0500

    Screen size and resultion is added to user settings, we should be pulling from the settings object from now on for screen dimensions

--- src/userSettings.h
+++ src/userSettings.h
@@ -3,16 +3,42 @@
 
 #include <stdexcept>
 
+enum Resolution {
+    _480P,
+    _720P,
+    _1080P,
+    _1440P,
+    _3840P
+};
+
+
 class userSettings {
     private:
         float volume;
+        int screenX;
+        int screenY;
+        enum Resolution resolution;
 
     public:
-        userSettings(float defaultVolume=0.0f); // Set default volume to 0 because in repeat testing its very annoying
+        userSettings(
+            float defaultVolume=0.0f, 
+            int defaultScreenX=1280, 
+            int defaultScreenY=720, 
+            enum Resolution defaultResolution = _720P); // Set default volume to 0 because in repeat testing its very annoying
         // NOTE: Change default volume to 1.0f before release
 
         void setVolume(float vol);
         float getVolume();
+
+        void setScreenX(int n);
+        int getScreenX();
+
+        void setScreenY(int n);
+        int getScreenY();
+
+        void setResolution(enum Resolution res);
+        enum Resolution getResolution();
+
 };
 
 
